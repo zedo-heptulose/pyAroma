bac45b5d20a471311c384c85b75ef2c9771c39c5
zedo-heptulose <baylissgage@gmail.com>
2025-04-24 03:08:20 -0400

    fixed

--- src/.ipynb_checkpoints/mathUtilities-checkpoint.py
+++ src/.ipynb_checkpoints/mathUtilities-checkpoint.py
@@ -19,8 +19,7 @@ def align_matrix(_v1, _v2, **kwargs):
     if np.allclose(v1,-v2,atol=1e-2):
         if debug:
             print('vectors are antiparallel')
-        #copilot code check later
-        #construct rotation about arbitrary axis
+    
         z_axis = np.array([0,0,1])
         y_axis = np.array([0,1,0])
         rot_ax = np.cross(v1, z_axis)
@@ -44,12 +43,11 @@ def align_matrix(_v1, _v2, **kwargs):
         
     
     angle_between = angle_about_axis(v1, v2, axis)
-    #HERE behaves unpredictably. returns align where it should antialign, etc.
+    
     par_angle = - angle_between
-    ##print (axis)
+    
     par_matrix = rotation_about_axis(axis, par_angle)
     
-    #print ('gets to end')
     return par_matrix
 
 
@@ -87,48 +85,9 @@ def angle_about_axis(_v1, _v2, _axis, **kwargs):
         raise ValueError('Axis not parallel to cross product of v1 and v2')
 
 
-# def rotation_about_axis(axis, angle):
-#     """
-#     chatgpt code
-    
-#     Calculate the rotation matrix from axis and angle.
-
-#     Args:
-#     axis: The rotation axis as a numpy array.
-#     angle: The rotation angle in radians.
-
-#     Returns:
-#     The rotation matrix.
-#     """
-#     # Normalize the axis
-#     axis = axis / np.linalg.norm(axis)
-
-#     # If the angle is zero (or very close), the vectors are already parallel
-#     if np.isclose(angle, 0):
-#         return np.eye(3)
-
-#     # If the angle is pi (or very close), the vectors are antiparallel
-#     if np.isclose(angle, np.pi):
-#         return -np.eye(3)
-
-#     # Calculate the quaternion components
-#     w = np.cos(angle / 2)
-#     x, y, z = axis * np.sin(angle / 2)
-
-#     # Calculate the rotation matrix
-#     rotation_matrix = np.array([
-#         [1 - 2*y*y - 2*z*z, 2*x*y - 2*z*w, 2*x*z + 2*y*w],
-#         [2*x*y + 2*z*w, 1 - 2*x*x - 2*z*z, 2*y*z - 2*x*w],
-#         [2*x*z - 2*y*w, 2*y*z + 2*x*w, 1 - 2*x*x - 2*y*y]
-#     ])
-
-#     return rotation_matrix
-
 
 def rotation_about_axis(_axis, angle):
-    """
-    unit tests.... these linear transformations would be a great thing to learn and apply unit testing for, no?
-    
+    """    
     Calculate the rotation matrix from axis and angle using Rodrigues' rotation formula.
 
     Args:
@@ -138,9 +97,8 @@ def rotation_about_axis(_axis, angle):
     Returns:
     The rotation matrix.
     """
-    # Normalize the axis
-    ###print(_axis)
-    # print(f'angle between: {angle}')
+    # chatGPT code, this whole function. look here if something goes wrong (hasn't yet!)
+    # very minor numerical instabilities but nothing that should meaningfully affect results
     
     axis = _axis.copy()
     if np.allclose(axis, np.array([0,0,0])):
